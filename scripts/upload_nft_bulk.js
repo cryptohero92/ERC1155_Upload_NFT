679869d079c01b0dd52448fef2aa76128928c383
cryptohero92 <alwaysflexibleman@gmail.com>
2022-12-21 15:46:15 +0900

    metadata upload to ipfs using Moralis after image upload

--- scripts/upload_nft_bulk.js
+++ scripts/upload_nft_bulk.js
@@ -28,7 +28,25 @@ Promise.all(promises).then(async () => {
     await Moralis.start({
         apiKey: process.env.MORALIS_API_KEY
     });
-    const response = await Moralis.EvmApi.ipfs.uploadFolder({
+    let response = await Moralis.EvmApi.ipfs.uploadFolder({
+        abi
+    });
+    // console.log(response?.toJSON());
+
+    // now create metadata per ipfs image and upload it again.
+    const images_json = response?.toJSON();
+    abi = [];
+    images_json.forEach((image_info, index) => {
+        abi.push({
+            path: `metadata/${index}.json`,
+            content: {
+                image: image_info.path,
+                name: `Dog NFT #${index}`,
+                description: `My Test NFT #${index} using for ERC1155`
+            }
+        })
+    });
+    response = await Moralis.EvmApi.ipfs.uploadFolder({
         abi
     });
     console.log(response?.toJSON());
